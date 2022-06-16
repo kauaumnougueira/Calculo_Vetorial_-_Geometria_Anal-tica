134018afa2cfc3e73bbdf1e3860210ea667fa337
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-16 17:45:46 -0300

    projecao() adcionado

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ vetor2 = Vetor(4, 5, 6)
 print(vetor1.create_and_see_vetor())
 print(vetor2.create_and_see_vetor())
 
+#PARA DOIS VETORES
 print(adicao(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
 print(subtracao(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
-print(prod_esc(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
-print(prod_vet(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
+print(prod_escalar(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
+print(prod_vetorial(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
+print(projecao(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor()))
+
+#PARA TRES VETORES
+vetor3 = Vetor(7, 8, 9)
+print(prod_misto(vetor1.create_and_see_vetor(), vetor2.create_and_see_vetor(), vetor3.create_and_see_vetor()))
--- operacoes_vetoriais/Operacoes.py
+++ operacoes_vetoriais/Operacoes.py
@@ -11,13 +11,13 @@ def subtracao(vetor1, vetor2):
     return result
 
 #PRODUTO ESCALAR
-def prod_esc(vetor1, vetor2):
+def prod_escalar(vetor1, vetor2):
     result = [vetor_1 * vetor_2 for vetor_1, vetor_2 in zip(vetor1, vetor2)]
     result_esc = sum(result)
     return result_esc
 
 #PRODUTO VETORIAL
-def prod_vet(vetor1, vetor2):
+def prod_vetorial(vetor1, vetor2):
     '''
     0 1 2 0 1
     i j k i j
@@ -28,3 +28,17 @@ def prod_vet(vetor1, vetor2):
     vetor_aux2 = [vetor1[2] * vetor2[1], vetor1[0] * vetor2[2], vetor1[1] * vetor2[0]]
     result = [vetor_aux1 - vetor_aux2 for vetor_aux1, vetor_aux2 in zip(vetor_aux1, vetor_aux2)]
     return result
+
+#PRODUTO MISTO
+def prod_misto(vetor1, vetor2, vetor3):
+    #VETOR1 X (VETOR3 X VETOR2)   
+    result = prod_escalar(prod_vetorial(vetor3, vetor2), vetor1)
+    return result
+
+#PROJEÇÃO
+def projecao(vetor1, vetor2):
+    #DE VETOR1 EM VETOR2
+    first_step = (prod_escalar(vetor1, vetor2)/ prod_escalar(vetor2, vetor2))
+    result = [first_step * vetor2[0], first_step * vetor2[1], first_step * vetor2[2]]
+    return result
+
