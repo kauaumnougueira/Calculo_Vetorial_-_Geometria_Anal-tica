c5268f2e7a191eaf15f350f0f782d1bf59e85348
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-16 15:25:31 -0300

    class Vetor criada

---
+++ main.py
@@ -0,0 +1,7 @@
+from operacoes_vetoriais.Vetor import *
+
+
+print("Cálculo Vetorial e Geometria Analítica")
+
+vetor = Vetor(1, 2, 3)
+print(vetor.create_and_see_vetor())
---
+++ operacoes_vetoriais/Vetor.py
@@ -0,0 +1,16 @@
+class Vetor():
+
+    def __init__(self, i, j, k):
+        self.i = i
+        self.j = j
+        self.k = k
+   
+    def create_and_see_vetor(self):
+        vetor = []
+        vetor.append(self.i)
+        vetor.append(self.j)
+        vetor.append(self.k)
+        return vetor
+
+
+    
