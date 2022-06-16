e8060e7c877166120fd2b4b00076d5685bb0a968
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-16 19:52:29 -0300

    volume tetraedro adcionado

--- operacoes_vetoriais/geometria/Areas.py
+++ operacoes_vetoriais/geometria/Areas.py
@@ -1,5 +1,10 @@
 from Operacoes import *
 
+'''
+1- PARALELOGRAMO (7)
+2- TRIÂNGULO (13)
+'''
+
 #PARALELOGRAMO
 def paralelogramo(vetor1, vetor2):
     area = modulo_prod_vet(vetor1, vetor2)
---
+++ operacoes_vetoriais/geometria/Volumes.py
@@ -0,0 +1,10 @@
+from Operacoes import * 
+
+'''
+1- TETRAEDRO (7)
+'''
+
+#TETRAEDRO
+def tetraedro():
+    volume = prod_misto * 1/6
+    return volume
