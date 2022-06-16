f12b302dc01f20833a78369d0f3868d879d0adc6
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-16 17:25:58 -0300

    prod_vet() adcionado

--- operacoes_vetoriais/Operacoes.py
+++ operacoes_vetoriais/Operacoes.py
@@ -17,9 +17,14 @@ def prod_esc(vetor1, vetor2):
     return result_esc
 
 #PRODUTO VETORIAL
-def prod_vet():
+def prod_vet(vetor1, vetor2):
     '''
+    0 1 2 0 1
     i j k i j
     x y z x y
     (jz - yk, kx - iz, iy - jx)
     '''
+    vetor_aux1 = [vetor1[1] * vetor2[2], vetor1[2] * vetor2[0], vetor1[0] * vetor2[1]]
+    vetor_aux2 = [vetor1[2] * vetor2[1], vetor1[0] * vetor2[2], vetor1[1] * vetor2[0]]
+    result = [vetor_aux1 - vetor_aux2 for vetor_aux1, vetor_aux2 in zip(vetor_aux1, vetor_aux2)]
+    return result
