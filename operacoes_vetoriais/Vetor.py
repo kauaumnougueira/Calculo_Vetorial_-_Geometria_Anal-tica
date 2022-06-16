e88f70e2b939fa9421f758c461f2790963a17e2c
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-16 15:31:08 -0300

    comentando em vetores

--- operacoes_vetoriais/Vetor.py
+++ operacoes_vetoriais/Vetor.py
@@ -1,10 +1,12 @@
 class Vetor():
 
+    #CONSTRUTOR DA CLASS VETOR
     def __init__(self, i, j, k):
         self.i = i
         self.j = j
         self.k = k
    
+    #CRIA E MOSTRA UM VETOR
     def create_and_see_vetor(self):
         vetor = []
         vetor.append(self.i)
